c618b89e5c9038bf6f852d75316eabe0365e6f94
rem1776 <Ryan.Mulhall@lscamd50-d.gfdl.noaa.gov>
2020-12-02 12:12:54 -0500

    Added subroutine to reorder data and changed indices order
    Former-commit-id: b79c0457b8ae9212d96d3c9501bbe21e434079f1

--- test_fms/mpp/test_global_arrays.F90
+++ test_fms/mpp/test_global_arrays.F90
@@ -19,7 +19,7 @@
 !> @author Ryan Mulhall
 !> @email gfdl.climate.model.info@noaa.gov
 !> @brief Unit tests for mpp global max, min, and sum functions
-!> @description Generates a random data set for both sizes of reals and ints
+!> @description Generates a random data set for both SIZEs of reals and ints
 !> then checks routines with local results received from each pe
 program test_global_arrays
 
@@ -40,19 +40,19 @@ program test_global_arrays
     
   integer, parameter            :: length=64
   integer                       :: id, pe, npes, root, i, j
-  integer(i4_kind)              :: maxI4, minI4, ierr, sumI4
-  integer(i8_kind)              :: maxI8, minI8, sumI8
+  integer(i4_kind)              :: maxI4, minI4, ierr, sumI4, sumI4_5d
+  integer(i8_kind)              :: maxI8, minI8, sumI8, sumI8_5d
   integer(i4_kind), allocatable :: dataI4(:,:), dataI4_5d(:,:,:,:,:)
   integer(i8_kind), allocatable :: dataI8(:,:), dataI8_5d(:,:,:,:,:)
   real(r4_kind), allocatable    :: dataR4(:,:)
   real(r8_kind), allocatable    :: dataR8(:,:)
   real, allocatable             :: rands(:)
   type(domain2D)                :: domain
-  real(r8_kind)                 :: rcoef, maxR8, minR8
-  real(r4_kind)                 :: maxR4, minR4
+  real(r8_kind)                 :: rcoef, maxR8, minR8, sumR8
+  real(r4_kind)                 :: maxR4, minR4, sumR4
   integer                       :: isc, iec, jsc, jec
   integer                       :: isd, ied, jsd, jed 
-  character(len=32)             :: strMax, strMin
+  character(len=32)             :: strTmp1, strTmp2
   integer(i4_kind), parameter   :: randmaxI4 = 2048
   integer(i8_kind), parameter   :: randmaxI8 = 4096
 
@@ -67,10 +67,10 @@ program test_global_arrays
 
   !> define domains and allocate
   call mpp_define_domains( (/1,length,1,length/), (/4,2/), domain, xhalo=0)
-  call mpp_get_compute_domain(domain, isc, iec, jsc, jec)
-  call mpp_get_data_domain(domain, isd, ied, jsd, jed)
-  allocate(dataI4(isd:ied, jsd:jed),dataI8(isd:ied, jsd:jed), rands(length*length))
-  allocate(dataR4(isd:ied, jsd:jed), dataR8(isd:ied, jsd:jed))
+  call mpp_get_compute_domain(domain, jsc, jec, isc, iec)
+  call mpp_get_data_domain(domain, jsd, jed, isd, ied)
+  allocate(dataI4(jsd:jed, isd:ied),dataI8(jsd:jed, isd:ied), rands(length*length))
+  allocate(dataR4(jsd:jed, isd:ied), dataR8(jsd:jed, isd:ied))
 
   !> make random arrays
   call random_seed()
@@ -78,10 +78,10 @@ program test_global_arrays
   do i=isc, iec-1
     do j=jsc, jec-1
       rcoef = rands(j + i*length) * 2 -1
-      dataI4(i, j) = int(rcoef * randmaxI4, kind=i4_kind)
-      dataI8(i, j) = int(rcoef * randmaxI8, kind=i8_kind)
-      dataR4(i, j) = real(rcoef, kind=r4_kind)
-      dataR8(i, j) = real(rcoef, kind=r8_kind)
+      dataI4(j, i) = int(rcoef * randmaxI4, kind=i4_kind)
+      dataI8(j, i) = int(rcoef * randmaxI8, kind=i8_kind)
+      dataR4(j, i) = real(rcoef, kind=r4_kind)
+      dataR8(j, i) = real(rcoef, kind=r8_kind)
     end do
   end do
 
@@ -90,76 +90,107 @@ program test_global_arrays
   call mpp_update_domains(dataI4, domain)
   maxI4 = mpp_global_max(domain, dataI4)
   minI4 = mpp_global_min(domain, dataI4)
-  write(strMax, *) maxI4
-  write(strMin, *) minI4
+  write(strTmp1, *) maxI4
+  write(strTmp2, *) minI4
   if(.NOT. checkResultInt4((/minI4, maxI4 /))) then
     call mpp_error(FATAL, "test_global_arrays: invalid 32-bit integer results"// &
-                               NEW_LINE('a')//"Max: "//strMax//" Min: "//strMin )
+                               NEW_LINE('a')//"Max: "//strTmp1//" Min: "//strTmp2 )
   endif
   call mpp_sync()
   call mpp_error(NOTE, "----------Testing 64-bit int mpp_global_max and mpp_global_min----------")
   call mpp_update_domains(dataI8, domain)
   maxI8 = mpp_global_max(domain, dataI8)
   minI8 = mpp_global_min(domain, dataI8)
-  write(strMax, *) maxI8
-  write(strMin, *) minI8
+  write(strTmp1, *) maxI8
+  write(strTmp2, *) minI8
   if(.NOT. checkResultInt8((/minI8, maxI8 /))) then
     call mpp_error(FATAL, "test_global_arrays: invalid 64-bit integer results"// &
-                               NEW_LINE('a')//"Max: "//strMax//" Min: "//strMin )
+                               NEW_LINE('a')//"Max: "//strTmp1//" Min: "//strTmp2 )
   endif
   call mpp_sync()
   call mpp_error(NOTE, "----------Testing 32-bit real mpp_global_max and mpp_global_min----------")
   call mpp_update_domains(dataR4, domain)
   maxR4 = mpp_global_max(domain, dataR4)
   minR4 = mpp_global_min(domain, dataR4)
-  write(strMax, *) maxR4
-  write(strMin, *) minR4
+  write(strTmp1, *) maxR4
+  write(strTmp2, *) minR4
   if(.NOT. checkResultReal4((/minR4, maxR4 /))) then
     call mpp_error(FATAL, "test_global_arrays: invalid 32-bit real results"// &
-                               NEW_LINE('a')//"Max: "//strMax//" Min: "//strMin )
+                               NEW_LINE('a')//"Max: "//strTmp1//" Min: "//strTmp2 )
   endif
   call mpp_sync()
   call mpp_error(NOTE, "----------Testing 64-bit real mpp_global_max and mpp_global_min----------")
   call mpp_update_domains(dataR8, domain)
   maxR8 = mpp_global_max(domain, dataR8)
   minR8 = mpp_global_min(domain, dataR8)
-  write(strMax, *) maxR8
-  write(strMin, *) minR8
+  write(strTmp1, *) maxR8
+  write(strTmp2, *) minR8
   if(.NOT. checkResultReal8((/minR8, maxR8 /))) then
     call mpp_error(FATAL, "test_global_arrays: invalid 64-bit real results"// &
-                               NEW_LINE('a')//"Max: "//strMax//" Min: "//strMin )
+                               NEW_LINE('a')//"Max: "//strTmp1//" Min: "//strTmp2 )
   endif
 
   !> test global sums for each kind
+  call mpp_error(NOTE, "----------Testing 32-bit real mpp_global_sum----------")
+  call mpp_update_domains(dataR4, domain)
+  sumR4 = mpp_global_sum(domain, dataR4)
+  write(strTmp1,*) sumR4 
+  if(.NOT. checkSumReal4(sumR4)) then
+    call mpp_error(FATAL, "test_global_arrays: invalid 32-bit real sum"// &
+                               NEW_LINE('a')//"Sum: "// strTmp1 )
+  endif
+  call mpp_error(NOTE, "----------Testing 64-bit real mpp_global_sum----------")
+  call mpp_update_domains(dataR8, domain)
+  sumR8 = mpp_global_sum(domain, dataR8)
+  write(strTmp1,*) sumR8 
+  if(.NOT. checkSumReal8(sumR8)) then
+    call mpp_error(FATAL, "test_global_arrays: invalid 64-bit real sum"// &
+                               NEW_LINE('a')//"Sum: "// strTmp1 )
+  endif
   call mpp_error(NOTE, "----------Testing 32-bit integer mpp_global_sum----------")
   call mpp_update_domains(dataI4, domain)
   sumI4 = mpp_global_sum(domain, dataI4)
-  write(strMax,*) sumI4
+  write(strTmp1,*) sumI4
   if(.NOT. checkSumInt4(sumI4)) then
     call mpp_error(FATAL, "test_global_arrays: invalid 32-bit integer sum"// &
-                               NEW_LINE('a')//"Sum: "// strMax )
+                               NEW_LINE('a')//"Sum: "// strTmp1 )
   endif
   call mpp_error(NOTE, "----------Testing 64-bit integer mpp_global_sum----------")
   call mpp_update_domains(dataI8, domain)
   sumI8 = mpp_global_sum(domain, dataI8)
-  write(strMax,*) sumI8
+  write(strTmp1,*) sumI8
   if(.NOT. checkSumInt8(sumI8)) then
     call mpp_error(FATAL, "test_global_arrays: invalid 64-bit integer sum"// &
-                               NEW_LINE('a')//"Sum: "// strMax )
+                               NEW_LINE('a')//"Sum: "// strTmp1 )
   endif
 
-  !> copy with more ranks and test sum
-  allocate(dataI4_5d(isd:ied,jsd:jed, 1, 1, 1), dataI8_5d(isd:ied,jsd:jed, 1, 1, 1))
-  do i=isc, iec-1
-    do j=jsc, jec-1
-      dataI4_5d(i, j, 1, 1, 1) = dataI4(i, j) 
-      dataI8_5d(i, j, 1, 1, 1) = dataI8(i, j) 
+  !> shuffle data ordering and copy into array with 5 ranks
+  call shuffleDataI4()
+  call shuffleDataI8()
+  allocate(dataI4_5d(jsd:jed, isd:ied, 1, 1, 1), dataI8_5d(jsd:jec,isd:ied, 1, 1, 1))
+  do i=isc, iec
+    do j=jsc, jec
+      dataI4_5d(j, i, 1, 1, 1) = dataI4(j, i)
+      dataI8_5d(j, i, 1, 1, 1) = dataI8(j, i)
     end do
   end do
   call mpp_sync()
-  call mpp_error(NOTE, "----------Testing 5D integer mpp_global_sum----------")
-  if( sumI4 .ne. mpp_global_sum(domain,dataI4_5d) .or. sumI8 .ne. mpp_global_sum(domain,dataI8_5d)) then
-    call mpp_error(FATAL, "test_global_arrays: invalid higher rank results")
+
+  call mpp_error(NOTE, "----------Testing integer mpp_global_sum with 5 ranks and reordering----------")
+  call mpp_update_domains(dataI4_5d, domain)
+  sumI4_5d = mpp_global_sum(domain, dataI4_5d)
+  write(strTmp1,*) sumI4_5d
+  if( sumI4 .ne. sumI4_5d ) then
+    call mpp_error(FATAL,"test_global_arrays: invalid 32-bit answer after reordering"// &
+                        NEW_LINE('a')//"Sum: "// strTmp1 )
+  endif
+
+  call mpp_update_domains(dataI8_5d, domain)
+  sumI8_5d = mpp_global_sum(domain, dataI8_5d)
+  write(strTmp1,*) sumI8_5d
+  if( sumI8 .ne. sumI8_5d) then
+    call mpp_error(FATAL,"test_global_arrays: invalid 64-bit answer after reordering"// &
+                        NEW_LINE('a')//"Sum: "// strTmp1 )
   endif
 
   deallocate(dataI4, dataI8, dataR4, dataR8, rands, dataI4_5d, dataI8_5d)
@@ -193,7 +224,6 @@ function checkResultInt4(res)
   deallocate(tres)
 end function checkResultInt4 
 
-!> true if all pes return the same result and have a lower/higher local max/min 
 function checkResultInt8(res)
   logical                               :: checkResultInt8
   integer(i8_kind),intent(in)           :: res(2)
@@ -218,7 +248,6 @@ function checkResultInt8(res)
   deallocate(tres)
 end function checkResultInt8
 
-!> true if all pes return the same result and have a lower/higher local max/min 
 function checkResultReal4(res)
   logical                            :: checkResultReal4
   real(r4_kind),intent(in)           :: res(2)
@@ -243,7 +272,6 @@ function checkResultReal4(res)
   deallocate(tres)
 end function checkResultReal4
 
-!> true if all pes return the same result and have a lower/higher local max/min 
 function checkResultReal8(res)
   logical                            :: checkResultReal8
   real(r8_kind),intent(in)           :: res(:)
@@ -267,6 +295,73 @@ function checkResultReal8(res)
   end if
   deallocate(tres)
 end function checkResultReal8
+
+!>@brief Sum local sums from pes and compares with gsum
+!>@return True if gsum is the global sum, false otherwise
+function checkSumReal4(gsum)
+  logical                   :: checkSumReal4
+  real(r4_kind),intent(in)  :: gsum
+  real(r4_kind),allocatable :: recv(:) !> pe's local sum at 1, global sum at 2
+  real(r4_kind)             :: nsum
+  integer                   :: i
+
+  allocate(recv(2))
+  ! root receives and sums local sums from each pe
+  if(pe .eq. root) then
+    nsum = SUM(dataR4)
+    do i=1, npes - 1 
+      call mpp_recv(recv, 2, i)
+      nsum = nsum + recv(1)
+      ! also check for matching global sum
+      if( recv(2) .ne. gsum ) then
+        checkSumReal4 = .false.
+        deallocate(recv)
+        return 
+      endif
+    end do
+    checkSumReal4 = nsum .eq. gsum
+  else
+    recv(1) = SUM(dataR4)
+    recv(2) = gsum 
+    call mpp_send(recv, 2, root) 
+    checkSumReal4 = .true. 
+  endif
+  deallocate(recv)
+end function checkSumReal4
+
+!>@brief Sum local sums from pes and compares with gsum
+!>@return True if gsum is the global sum, false otherwise
+function checkSumReal8(gsum)
+  logical                   :: checkSumReal8
+  real(r8_kind),intent(in)  :: gsum
+  real(r8_kind),allocatable :: recv(:) !> pe's local sum at 1, global sum at 2
+  real(r8_kind)             :: nsum
+  integer                   :: i
+
+  allocate(recv(2))
+  ! root receives and sums local sums from each pe
+  if(pe .eq. root) then
+    nsum = SUM(dataR8)
+    do i=1, npes - 1 
+      call mpp_recv(recv, 2, i)
+      nsum = nsum + recv(1)
+      ! also check for matching global sum
+      if( recv(2) .ne. gsum ) then
+        checkSumReal8 = .false.
+        deallocate(recv)
+        return 
+      endif
+    end do
+    checkSumReal8 = nsum .eq. gsum
+  else
+    recv(1) = SUM(dataR8)
+    recv(2) = gsum 
+    call mpp_send(recv, 2, root) 
+    checkSumReal8 = .true. 
+  endif
+  deallocate(recv)
+end function checkSumReal8
+
 !>@brief Sum local sums from pes and compares with gsum
 !>@return True if gsum is the global sum, false otherwise
 function checkSumInt4(gsum)
@@ -326,11 +421,103 @@ function checkSumInt8(gsum)
     checkSumInt8 = nsum .eq. gsum
   else
     recv(1) = SUM(dataI8)
-    recv(2) = gsum 
-    call mpp_send(recv, 2, root) 
-    checkSumInt8 = .true. 
+    recv(2) = gsum
+    call mpp_send(recv, 2, root)
+    checkSumInt8 = .true.
   endif
   deallocate(recv)
 end function checkSumInt8
+
+!> aggregates data on root and randomizes ordering, then sends partitions back to pes
+subroutine shuffleDataI4
+  integer(i4_kind), allocatable :: trans(:,:), shuffled(:), tmp
+  integer                       :: rind, tsum=0
+
+  allocate(trans(SIZE(dataI4,1), SIZE(dataI4,2)))
+  allocate(shuffled(1:length*length))
+
+  if( pe.eq.root) then
+    !> get array partitions and aggregate into 1d
+    shuffled(1:SIZE(dataI4)) = RESHAPE(dataI4, (/SIZE(dataI4)/))
+    do i=1, npes-1
+      call mpp_recv(trans, SIZE(dataI4) , i)
+      shuffled( SIZE(trans)*i+1 : SIZE(trans)*(i+1)) = RESHAPE(trans, (/SIZE(trans)/))
+    end do
+
+    !> shuffle order
+    do i=1, length*length
+      rind = (rands(i) * length * length)
+      if( rind .eq. 0) then
+        rind = 1
+      endif
+      tmp = shuffled(i)
+      shuffled(i) = shuffled(rind)
+      shuffled(rind) = tmp
+    end do
+    trans = 0
+
+    !> send back to pes
+    do i=0, npes-1
+      trans = RESHAPE(shuffled(SIZE(trans)*i + 1:SIZE(trans)*(i+1)), &
+                         (/SIZE(trans,1), SIZE(trans,2) /) )
+      if(i.gt.0) then 
+        call mpp_send(trans, SIZE(trans), i)
+      else 
+        dataI4 = trans
+      endif
+    end do
+  else
+    call mpp_send(dataI4, SIZE(dataI4), root)
+    call mpp_recv(trans, SIZE(dataI4), root)
+    dataI4 = trans
+  endif
+  deallocate(trans, shuffled)
+end subroutine shuffleDataI4
+
+!> aggregates data on root and randomizes ordering, then sends partitions back to pes
+subroutine shuffleDataI8
+  integer(i8_kind), allocatable :: trans(:,:), shuffled(:), tmp
+  integer                       :: rind, tsum=0
+
+  allocate(trans(SIZE(dataI8,1), SIZE(dataI8,2)))
+  allocate(shuffled(1:length*length))
+
+  if( pe.eq.root) then
+    !> get array partitions and aggregate into 1d
+    shuffled(1:SIZE(dataI8)) = RESHAPE(dataI8, (/SIZE(dataI8)/))
+    do i=1, npes-1
+      call mpp_recv(trans, SIZE(dataI8) , i)
+      shuffled( SIZE(trans)*i+1 : SIZE(trans)*(i+1)) = RESHAPE(trans, (/SIZE(trans)/))
+    end do
+
+    !> shuffle order
+    do i=1, length*length
+      rind = (rands(i) * length * length)
+      if( rind .eq. 0) then
+        rind = 1
+      endif
+      tmp = shuffled(i)
+      shuffled(i) = shuffled(rind)
+      shuffled(rind) = tmp
+    end do
+    trans = 0
+
+    !> send back to pes
+    do i=0, npes-1
+      trans = RESHAPE(shuffled(SIZE(trans)*i + 1:SIZE(trans)*(i+1)), &
+                         (/SIZE(trans,1), SIZE(trans,2) /) )
+      if(i.gt.0) then 
+        call mpp_send(trans, SIZE(trans), i)
+      else 
+        dataI8 = trans
+      endif
+    end do
+  else
+    call mpp_send(dataI8, SIZE(dataI8), root)
+    call mpp_recv(trans, SIZE(dataI8), root)
+    dataI8 = trans
+  endif
+  deallocate(trans, shuffled)
+end subroutine shuffleDataI8
 
 end program test_global_arrays
