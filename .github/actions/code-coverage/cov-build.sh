431cf162acd3916426a7018b82c81e9e4a3899ed
rem1776 <Ryan.Mulhall@lscamd50-d.gfdl.noaa.gov>
2022-02-07 17:12:20 -0500

    Add internal action for code coverage

---
+++ .github/actions/code-coverage/cov-build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# simple build script
+mkdir coverage-build
+cd coverage-build
+echo "::notice:: generating configure script..."
+autoreconf -i ../configure.ac
+echo "::notice:: configuring build..."
+../configure --enable-code-coverage --enable-mixed-mode
+echo "::notice:: building and generating report..."
+make check-code-coverage
+# TODO read report data and output
+echo "::notice:: Report done"
