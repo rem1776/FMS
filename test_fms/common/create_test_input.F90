6bb15b75740c958880025ac504bd742679d52b4a
rem1776 <Ryan.Mulhall@noaa.gov>
2026-07-15 12:43:29 -0400

    consolidate cubic mosaic routines; refactor input file routines for reuse

---
+++ test_fms/common/create_test_input.F90
@@ -0,0 +1,309 @@
+module create_test_input_mod
+
+  use fms2_io_mod, only: open_file, close_file, FmsNetcdfFile_t
+  use fms2_io_mod, only: register_axis, register_field, write_data, register_variable_attribute
+  use fms2_io_mod, only: unlimited
+
+  implicit none
+
+contains
+
+  subroutine create_exchange_test_input()
+
+    call create_grid_spec()
+    call create_atmos_mosaic()
+    call create_atmos_hgrid(288, 180)
+    call create_land_mosaic()
+    call create_land_hgrids(180, 180)
+    call create_sst_ice_clim(360, 180, 180, 91)
+
+  end subroutine create_exchange_test_input
+
+  subroutine create_data_override_test_input(atmos_nx, atmos_ny, land_nx, land_ny, data_nx, data_ny, ice_nx, ice_ny)
+    integer, intent(in) :: atmos_nx, atmos_ny
+    integer, intent(in) :: land_nx, land_ny
+    integer, intent(in) :: data_nx, data_ny
+    integer, intent(in) :: ice_nx, ice_ny
+
+
+    call create_grid_spec()
+    call create_atmos_mosaic()
+    call create_atmos_hgrid(atmos_nx, atmos_ny)
+    call create_land_mosaic()
+    call create_land_hgrids(land_nx, land_ny)
+    call create_sst_ice_clim(data_nx, data_ny, ice_nx, ice_ny)
+
+  end subroutine create_data_override_test_input
+
+  subroutine create_grid_spec()
+    type(FmsNetcdfFile_t) :: fileobj
+    character(len=6), dimension(1) :: str_arr
+
+    if (open_file(fileobj, 'INPUT/grid_spec.nc', 'overwrite')) then
+      call register_axis(fileobj, 'str', 255)
+      call register_axis(fileobj, 'nfile_aXo', 1)
+      call register_axis(fileobj, 'nfile_aXl', 1)
+      call register_axis(fileobj, 'nfile_lXo', 1)
+
+      call register_field(fileobj, 'atm_mosaic_dir', 'char', (/'str'/))
+      call register_field(fileobj, 'atm_mosaic_file', 'char', (/'str'/))
+      call register_field(fileobj, 'atm_mosaic', 'char', (/'str'/))
+      call register_field(fileobj, 'lnd_mosaic_dir', 'char', (/'str'/))
+      call register_field(fileobj, 'lnd_mosaic_file', 'char', (/'str'/))
+      call register_field(fileobj, 'lnd_mosaic', 'char', (/'str'/))
+      call register_field(fileobj, 'ocn_mosaic_dir', 'char', (/'str'/))
+      call register_field(fileobj, 'ocn_mosaic_file', 'char', (/'str'/))
+      call register_field(fileobj, 'ocn_mosaic', 'char', (/'str'/))
+      call register_field(fileobj, 'ocn_topog_dir', 'char', (/'str'/))
+      call register_field(fileobj, 'ocn_topog_file', 'char', (/'str'/))
+      call register_field(fileobj, 'aXo_file', 'char', (/'str', 'nfile_aXo'/))
+      call register_field(fileobj, 'aXl_file', 'char', (/'str', 'nfile_aXl'/))
+      call register_field(fileobj, 'lXo_file', 'char', (/'str', 'nfile_lXo'/))
+
+      call write_data(fileobj, 'atm_mosaic_dir', './')
+      call write_data(fileobj, 'atm_mosaic_file', 'atmos_mosaic.nc')
+      call write_data(fileobj, 'atm_mosaic', 'atmos_mosaic')
+      call write_data(fileobj, 'lnd_mosaic_dir', './')
+      call write_data(fileobj, 'lnd_mosaic_file', 'unit_test_land_mosaic.nc')
+      call write_data(fileobj, 'lnd_mosaic', 'unit_test_land_mosaic')
+      call write_data(fileobj, 'ocn_mosaic_dir', './')
+      call write_data(fileobj, 'ocn_mosaic_file', 'atmos_mosaic.nc')
+      call write_data(fileobj, 'ocn_mosaic', 'ocean_mosaic')
+      call write_data(fileobj, 'ocn_topog_dir', './')
+      call write_data(fileobj, 'ocn_topog_file', 'topog.nc')
+      str_arr(1) = 'aXo.nc'
+      call write_data(fileobj, 'aXo_file', str_arr)
+      str_arr(1) = 'aXl.nc'
+      call write_data(fileobj, 'aXl_file', str_arr)
+      str_arr(1) = 'lXo.nc'
+      call write_data(fileobj, 'lXo_file', str_arr)
+
+      call close_file(fileobj)
+    end if
+  end subroutine create_grid_spec
+
+  subroutine create_atmos_mosaic()
+    type(FmsNetcdfFile_t) :: fileobj
+    character(len=38), dimension(1) :: gridfiles, gridtiles
+
+    if (open_file(fileobj, 'INPUT/atmos_mosaic.nc', 'overwrite')) then
+      call register_axis(fileobj, 'ntiles', 1)
+      call register_axis(fileobj, 'string', 50)
+
+      call register_field(fileobj, 'mosaic', 'char', (/'string'/))
+      call register_field(fileobj, 'gridlocation', 'char', (/'string'/))
+      call register_field(fileobj, 'gridfiles', 'char', dimensions=(/'string', 'ntiles'/))
+      call register_field(fileobj, 'gridtiles', 'char', dimensions=(/'string', 'ntiles'/))
+
+      call write_data(fileobj, 'mosaic', 'atmos_mosaic')
+      call write_data(fileobj, 'gridlocation', './')
+      gridfiles(1) = 'atmos_hgrid.nc'
+      gridtiles(1) = 'atmos'
+      call write_data(fileobj, 'gridfiles', gridfiles)
+      call write_data(fileobj, 'gridtiles', gridtiles)
+
+      call close_file(fileobj)
+    end if
+  end subroutine create_atmos_mosaic
+
+  subroutine create_atmos_hgrid(grid_nx, grid_ny)
+    integer, intent(in) :: grid_nx, grid_ny
+    type(FmsNetcdfFile_t) :: fileobj
+    real, allocatable :: x(:,:), y(:,:)
+    integer :: i, j
+
+    allocate(x(grid_nx+1, grid_ny+1))
+    allocate(y(grid_nx+1, grid_ny+1))
+
+    do i = 1, grid_nx+1
+      do j = 1, grid_ny+1
+        x(i,j) = real(i-1) * 360.0 / grid_nx
+        y(i,j) = real(j-1) * 180.0 / grid_ny - 90.0
+      end do
+    end do
+
+    if (open_file(fileobj, 'INPUT/atmos_hgrid.nc', 'overwrite')) then
+      call register_axis(fileobj, 'nx', grid_nx)
+      call register_axis(fileobj, 'ny', grid_ny)
+      call register_axis(fileobj, 'nxp', grid_nx+1)
+      call register_axis(fileobj, 'nyp', grid_ny+1)
+
+      call register_field(fileobj, 'x', 'double', (/'nxp', 'nyp'/))
+      call register_field(fileobj, 'y', 'double', (/'nxp', 'nyp'/))
+      call register_field(fileobj, 'area', 'double', (/'nx', 'ny'/))
+
+      call write_data(fileobj, 'x', x)
+      call write_data(fileobj, 'y', y)
+
+      call close_file(fileobj)
+    end if
+
+    deallocate(x, y)
+  end subroutine create_atmos_hgrid
+
+  subroutine create_land_mosaic()
+    type(FmsNetcdfFile_t) :: fileobj
+    character(len=255), dimension(6) :: gridfiles, gridtiles
+
+    gridfiles = (/'unit_test_land_hgrid.tile1.nc', &
+                  'unit_test_land_hgrid.tile2.nc', &
+                  'unit_test_land_hgrid.tile3.nc', &
+                  'unit_test_land_hgrid.tile4.nc', &
+                  'unit_test_land_hgrid.tile5.nc', &
+                  'unit_test_land_hgrid.tile6.nc'/)
+    gridtiles = (/'tile1', 'tile2', 'tile3', 'tile4', 'tile5', 'tile6'/)
+
+    if (open_file(fileobj, 'INPUT/unit_test_land_mosaic.nc', 'overwrite')) then
+      call register_axis(fileobj, 'ntiles', 6)
+      call register_axis(fileobj, 'string', 255)
+
+      call register_field(fileobj, 'mosaic', 'char', (/'string'/))
+      call register_field(fileobj, 'gridfiles', 'char', (/'string', 'ntiles'/))
+      call register_field(fileobj, 'gridtiles', 'char', (/'string', 'ntiles'/))
+
+      call write_data(fileobj, 'mosaic', 'land_mosaic')
+      call write_data(fileobj, 'gridfiles', gridfiles)
+      call write_data(fileobj, 'gridtiles', gridtiles)
+
+      call close_file(fileobj)
+    end if
+  end subroutine create_land_mosaic
+
+  subroutine create_land_hgrids(land_nx, land_ny)
+    integer, intent(in) :: land_nx, land_ny
+    type(FmsNetcdfFile_t) :: fileobj
+    real, allocatable :: x(:,:), y(:,:)
+    character(len=35) :: filename
+    integer :: tile, i, j
+
+    allocate(x(land_nx+1, land_ny+1))
+    allocate(y(land_nx+1, land_ny+1))
+
+    do i = 1, land_nx+1
+      do j = 1, land_ny+1
+        x(i,j) = real(i-1) * 360.0 / land_nx
+        y(i,j) = real(j-1) * 180.0 / land_ny - 90.0
+      end do
+    end do
+
+    if (open_file(fileobj, 'INPUT/unit_test_land_hgrid.nc', 'overwrite')) then
+      call register_axis(fileobj, 'nx', land_nx)
+      call register_axis(fileobj, 'ny', land_ny)
+      call register_axis(fileobj, 'nxp', land_nx+1)
+      call register_axis(fileobj, 'nyp', land_ny+1)
+
+      call register_field(fileobj, 'x', 'double', (/'nxp', 'nyp'/))
+      call register_field(fileobj, 'y', 'double', (/'nxp', 'nyp'/))
+
+      call write_data(fileobj, 'x', x)
+      call write_data(fileobj, 'y', y)
+
+      call close_file(fileobj)
+    end if
+
+    do tile = 1, 6
+      write(filename, '(A,I1,A)') 'INPUT/unit_test_land_hgrid.tile', tile, '.nc'
+
+      if (open_file(fileobj, trim(filename), 'overwrite')) then
+        call register_axis(fileobj, 'nx', land_nx)
+        call register_axis(fileobj, 'ny', land_ny)
+        call register_axis(fileobj, 'nxp', land_nx+1)
+        call register_axis(fileobj, 'nyp', land_ny+1)
+
+        call register_field(fileobj, 'x', 'double', (/'nxp', 'nyp'/))
+        call register_field(fileobj, 'y', 'double', (/'nxp', 'nyp'/))
+        call register_field(fileobj, 'area', 'double', (/'nx', 'ny'/))
+
+        call write_data(fileobj, 'x', x)
+        call write_data(fileobj, 'y', y)
+
+        call close_file(fileobj)
+      end if
+    end do
+
+    deallocate(x, y)
+  end subroutine create_land_hgrids
+
+  subroutine create_sst_ice_clim(data_nx, data_ny, ice_nx, ice_ny)
+    integer, intent(in) :: data_nx, data_ny, ice_nx, ice_ny
+    type(FmsNetcdfFile_t) :: fileobj
+    real, allocatable :: sst(:,:,:), sic(:,:,:), sit(:,:,:)
+    double precision, allocatable :: lon(:), lat(:), time(:), lon_ice(:), lat_ice(:)
+    integer :: i, t
+
+    allocate(lon(data_nx))
+    allocate(lat(data_ny))
+    allocate(lon_ice(ice_nx))
+    allocate(lat_ice(ice_ny))
+    allocate(time(12))
+    allocate(sst(data_nx, data_ny, 12))
+    allocate(sic(data_nx, data_ny, 12))
+    allocate(sit(ice_nx, ice_ny, 12))
+
+    do i = 1, data_nx
+      lon(i) = real(i-1) * 360.0 / data_nx - 180.0
+    end do
+    do i = 1, data_ny
+      lat(i) = real(i-1) * 180.0 / data_ny - 90.0
+    end do
+    do i = 1, ice_nx
+      lon_ice(i) = real(i-1) * 360.0 / ice_nx - 180.0
+    end do
+    do i = 1, ice_ny
+      lat_ice(i) = real(i-1) * 180.0 / max(ice_ny - 1, 1) - 90.0
+    end do
+    do t = 1, 12
+      time(t) = real(t-1) * 30.0
+    end do
+
+    sst = 20.0
+    sic = 0.0
+    sit = 0.0
+
+    if (open_file(fileobj, 'INPUT/sst_ice_clim.nc', 'overwrite')) then
+      call register_axis(fileobj, 'LONGITUDE', data_nx)
+      call register_axis(fileobj, 'LATITUDE', data_ny)
+      call register_axis(fileobj, 'LON_ICE', ice_nx)
+      call register_axis(fileobj, 'LAT', ice_ny)
+      call register_axis(fileobj, 'TIME', unlimited)
+
+      call register_field(fileobj, 'LONGITUDE', 'double', (/'LONGITUDE'/))
+      call register_field(fileobj, 'LATITUDE', 'double', (/'LATITUDE'/))
+      call register_field(fileobj, 'LON_ICE', 'double', (/'LON_ICE'/))
+      call register_field(fileobj, 'LAT', 'double', (/'LAT'/))
+      call register_field(fileobj, 'TIME', 'double', (/'TIME'/))
+      call register_field(fileobj, 'SST', 'float', (/'LONGITUDE', 'LATITUDE ', 'TIME      '/))
+      call register_field(fileobj, 'SIC', 'float', (/'LONGITUDE', 'LATITUDE ', 'TIME      '/))
+      call register_field(fileobj, 'SIT', 'float', (/'LON_ICE   ', 'LAT       ', 'TIME      '/))
+
+      call register_variable_attribute(fileobj, 'LONGITUDE', 'units', 'degrees_east', str_len=12)
+      call register_variable_attribute(fileobj, 'LATITUDE', 'units', 'degrees_north', str_len=13)
+      call register_variable_attribute(fileobj, 'LON_ICE', 'units', 'degrees_east', str_len=12)
+      call register_variable_attribute(fileobj, 'LAT', 'units', 'degrees_north', str_len=13)
+      call register_variable_attribute(fileobj, 'TIME', 'units', 'days since 0002-01-01 00:00:00', str_len=30)
+      call register_variable_attribute(fileobj, 'TIME', 'calendar', 'noleap', str_len=6)
+      call register_variable_attribute(fileobj, 'SST', 'long_name', 'Sea surface temperature', str_len=23)
+      call register_variable_attribute(fileobj, 'SST', 'units', 'K', str_len=1)
+      call register_variable_attribute(fileobj, 'SIC', 'long_name', 'Sea-ice concentration', str_len=21)
+      call register_variable_attribute(fileobj, 'SIC', 'units', '%', str_len=1)
+      call register_variable_attribute(fileobj, 'SIT', 'long_name', 'sea ice thickness', str_len=17)
+      call register_variable_attribute(fileobj, 'SIT', 'units', 'm', str_len=1)
+
+      call write_data(fileobj, 'LONGITUDE', lon)
+      call write_data(fileobj, 'LATITUDE', lat)
+      call write_data(fileobj, 'LON_ICE', lon_ice)
+      call write_data(fileobj, 'LAT', lat_ice)
+      call write_data(fileobj, 'TIME', time)
+      call write_data(fileobj, 'SST', sst)
+      call write_data(fileobj, 'SIC', sic)
+      call write_data(fileobj, 'SIT', sit)
+
+      call close_file(fileobj)
+    end if
+
+    deallocate(lon, lat, lon_ice, lat_ice, time, sst, sic, sit)
+  end subroutine create_sst_ice_clim
+
+end module create_test_input_mod
+
+  
--- test_fms/common/test_fms.F90
+++ test_fms/common/test_fms.F90
@@ -24,6 +24,7 @@
 module fms_test_mod
   use random_numbers_mod, only: randomNumberStream, initializeRandomNumberStream, getRandomNumbers
   use mpp_mod,         only: mpp_error, FATAL
+  use mpp_domains_mod, only: domain2d, mpp_define_mosaic
   use platform_mod
 
   implicit none
@@ -231,6 +232,110 @@ module fms_test_mod
 
     n = self%ub(i) - self%lb(i) + 1
   end function permutable_indices_n_4d
+
+  !> Define a six-tile cubic mosaic domain layout used by multiple tests.
+  subroutine define_cubic_mosaic(type, domain, ni, nj, global_indices, layout, pe_start, pe_end, use_memsize, halo)
+    character(len=*), intent(in)  :: type
+    type(domain2d), intent(inout) :: domain
+    integer,        intent(in)    :: global_indices(:,:), layout(:,:)
+    integer,        intent(in)    :: ni(:), nj(:)
+    integer,        intent(in)    :: pe_start(:), pe_end(:)
+    logical, optional, intent(in) :: use_memsize
+    integer, optional, intent(in) :: halo
+    integer, dimension(12)        :: istart1, iend1, jstart1, jend1, tile1
+    integer, dimension(12)        :: istart2, iend2, jstart2, jend2, tile2
+    integer                       :: ntiles, num_contact, msize(2)
+    logical                       :: use_memsize_local
+    integer                       :: whalo = 2, shalo = 2, ehalo = 2, nhalo = 2
+
+    use_memsize_local = .true.
+    if (present(use_memsize)) use_memsize_local = use_memsize
+    if (present(halo)) then
+    nhalo = halo
+    shalo = halo
+    whalo = halo
+    ehalo = halo
+    endif
+
+    ntiles = 6
+    num_contact = 12
+    if(size(pe_start(:)) .NE. 6 .OR. size(pe_end(:)) .NE. 6 ) call mpp_error(FATAL, &
+      "define_cubic_mosaic: size of pe_start and pe_end should be 6")
+    if(size(global_indices,1) .NE. 4) call mpp_error(FATAL, &
+      "define_cubic_mosaic: size of first dimension of global_indices should be 4")
+    if(size(global_indices,2) .NE. 6) call mpp_error(FATAL, &
+      "define_cubic_mosaic: size of second dimension of global_indices should be 6")
+    if(size(layout,1) .NE. 2) call mpp_error(FATAL, &
+      "define_cubic_mosaic: size of first dimension of layout should be 2")
+    if(size(layout,2) .NE. 6) call mpp_error(FATAL, &
+      "define_cubic_mosaic: size of second dimension of layout should be 6")
+    if(size(ni(:)) .NE. 6 .OR. size(nj(:)) .NE. 6) call mpp_error(FATAL, &
+      "define_cubic_mosaic: size of ni and nj should be 6")
+
+    ! Contact line 1, between tile 1 (EAST) and tile 2 (WEST)
+    tile1(1) = 1; tile2(1) = 2
+    istart1(1) = ni(1);  iend1(1) = ni(1);  jstart1(1) = 1;      jend1(1) = nj(1)
+    istart2(1) = 1;      iend2(1) = 1;      jstart2(1) = 1;      jend2(1) = nj(2)
+    ! Contact line 2, between tile 1 (NORTH) and tile 3 (WEST)
+    tile1(2) = 1; tile2(2) = 3
+    istart1(2) = 1;      iend1(2) = ni(1);  jstart1(2) = nj(1);  jend1(2) = nj(1)
+    istart2(2) = 1;      iend2(2) = 1;      jstart2(2) = nj(3);  jend2(2) = 1
+    ! Contact line 3, between tile 1 (WEST) and tile 5 (NORTH)
+    tile1(3) = 1; tile2(3) = 5
+    istart1(3) = 1;      iend1(3) = 1;      jstart1(3) = 1;      jend1(3) = nj(1)
+    istart2(3) = ni(5);  iend2(3) = 1;      jstart2(3) = nj(5);  jend2(3) = nj(5)
+    ! Contact line 4, between tile 1 (SOUTH) and tile 6 (NORTH)
+    tile1(4) = 1; tile2(4) = 6
+    istart1(4) = 1;      iend1(4) = ni(1);  jstart1(4) = 1;      jend1(4) = 1
+    istart2(4) = 1;      iend2(4) = ni(6);  jstart2(4) = nj(6);  jend2(4) = nj(6)
+    ! Contact line 5, between tile 2 (NORTH) and tile 3 (SOUTH)
+    tile1(5) = 2; tile2(5) = 3
+    istart1(5) = 1;      iend1(5) = ni(2);  jstart1(5) = nj(2);  jend1(5) = nj(2)
+    istart2(5) = 1;      iend2(5) = ni(3);  jstart2(5) = 1;      jend2(5) = 1
+    ! Contact line 6, between tile 2 (EAST) and tile 4 (SOUTH)
+    tile1(6) = 2; tile2(6) = 4
+    istart1(6) = ni(2);  iend1(6) = ni(2);  jstart1(6) = 1;      jend1(6) = nj(2)
+    istart2(6) = ni(4);  iend2(6) = 1;      jstart2(6) = 1;      jend2(6) = 1
+    ! Contact line 7, between tile 2 (SOUTH) and tile 6 (EAST)
+    tile1(7) = 2; tile2(7) = 6
+    istart1(7) = 1;      iend1(7) = ni(2);  jstart1(7) = 1;      jend1(7) = 1
+    istart2(7) = ni(6);  iend2(7) = ni(6);  jstart2(7) = nj(6);  jend2(7) = 1
+    ! Contact line 8, between tile 3 (EAST) and tile 4 (WEST)
+    tile1(8) = 3; tile2(8) = 4
+    istart1(8) = ni(3);  iend1(8) = ni(3);  jstart1(8) = 1;      jend1(8) = nj(3)
+    istart2(8) = 1;      iend2(8) = 1;      jstart2(8) = 1;      jend2(8) = nj(4)
+    ! Contact line 9, between tile 3 (NORTH) and tile 5 (WEST)
+    tile1(9) = 3; tile2(9) = 5
+    istart1(9) = 1;      iend1(9) = ni(3);  jstart1(9) = nj(3);  jend1(9) = nj(3)
+    istart2(9) = 1;      iend2(9) = 1;      jstart2(9) = nj(5);  jend2(9) = 1
+    ! Contact line 10, between tile 4 (NORTH) and tile 5 (SOUTH)
+    tile1(10) = 4; tile2(10) = 5
+    istart1(10) = 1;     iend1(10) = ni(4); jstart1(10) = nj(4); jend1(10) = nj(4)
+    istart2(10) = 1;     iend2(10) = ni(5); jstart2(10) = 1;     jend2(10) = 1
+    ! Contact line 11, between tile 4 (EAST) and tile 6 (SOUTH)
+    tile1(11) = 4; tile2(11) = 6
+    istart1(11) = ni(4); iend1(11) = ni(4); jstart1(11) = 1;     jend1(11) = nj(4)
+    istart2(11) = ni(6); iend2(11) = 1;     jstart2(11) = 1;     jend2(11) = 1
+    ! Contact line 12, between tile 5 (EAST) and tile 6 (WEST)
+    tile1(12) = 5; tile2(12) = 6
+    istart1(12) = ni(5); iend1(12) = ni(5); jstart1(12) = 1;     jend1(12) = nj(5)
+    istart2(12) = 1;     iend2(12) = 1;     jstart2(12) = 1;     jend2(12) = nj(6)
+
+    msize(1) = maxval(ni(:)/layout(1,:)) + whalo + ehalo + 1
+    msize(2) = maxval(nj(:)/layout(2,:)) + shalo + nhalo + 1
+
+    if(use_memsize_local) then
+    call mpp_define_mosaic(global_indices, layout, domain, ntiles, num_contact, tile1, tile2, &
+      istart1, iend1, jstart1, jend1, istart2, iend2, jstart2, jend2,      &
+      pe_start, pe_end, symmetry = .true., whalo=whalo, ehalo=ehalo,   &
+      shalo=shalo, nhalo=nhalo, name = trim(type), memory_size = msize  )
+    else
+    call mpp_define_mosaic(global_indices, layout, domain, ntiles, num_contact, tile1, tile2, &
+      istart1, iend1, jstart1, jend1, istart2, iend2, jstart2, jend2,      &
+      pe_start, pe_end, symmetry = .true., whalo=whalo, ehalo=ehalo,   &
+      shalo=shalo, nhalo=nhalo, name = trim(type) )
+    endif
+  end subroutine define_cubic_mosaic
 
   pure recursive function factorial(n) result(res)
     integer, intent(in) :: n
